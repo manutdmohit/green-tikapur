2b91440461bbba3a344c2b0ee625ade75f7877ec
Mohit Saud <mohit@prabidhilabs.com>
2024-05-23 18:37:44 +0545

    Added chakra-ui and add prev-experience page

---
+++ app/prev-experience/page.tsx
@@ -0,0 +1,12 @@
+import ExperienceTable from '@/components/PreviousExperience';
+
+const PrevExperiencePage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8 mt-5 mb-5">
+      <h1 className="text-3xl font-bold mb-4">अघिल्लो अनुभव</h1>
+      <ExperienceTable />
+    </div>
+  );
+};
+
+export default PrevExperiencePage;
